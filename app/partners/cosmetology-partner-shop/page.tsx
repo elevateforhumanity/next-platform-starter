07ca056aea584d9453a9ba72193062996e24f98e
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-05-29 18:52:03 -0400

    fix(routes): mark cosmetology redirect noindex

--- app/partners/cosmetology-partner-shop/page.tsx
+++ app/partners/cosmetology-partner-shop/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from 'next/navigation';
 
+export const metadata = {
+  robots: { index: false, follow: false },
+};
+
 export default function CosmetologyPartnerShopRedirect() {
   redirect('/partners/cosmetology-host-shop');
 }
