66c32526a3dadfd8da753a4eb14c467a20bc6f84
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-05-29 20:13:26 -0400

    fix(routes): remove old cosmetology partner shop URL

--- app/partners/cosmetology-partner-shop/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export const metadata = {
-  robots: { index: false, follow: false },
-};
-
-export default function CosmetologyPartnerShopRedirect() {
-  redirect('/partners/cosmetology-host-shop');
-}
