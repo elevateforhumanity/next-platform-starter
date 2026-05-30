51bb4168f1ff4394d26d59c88af1307ec463d02b
elevateforhumanity <Elizabethpowell6262@gmail.com>
2026-05-29 20:13:10 -0400

    fix(routes): remove old barbershop apprenticeship apply URL

--- app/partners/barbershop-apprenticeship/apply/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export const metadata = {
-  robots: { index: false, follow: false },
-};
-
-export default function BarbershopApprenticeshipApplyRedirect() {
-  redirect('/partners/barber-host-shop/apply');
-}
